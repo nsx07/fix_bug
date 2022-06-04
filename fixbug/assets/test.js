6a49d14a621f6ed1d631a33b247bc396c0b9f333
nsx07 <felipeaugustodluz@hotmail.com>
2022-06-04 03:57:37 -0300

    update sign in and login

--- fixbug/assets/test.js
+++ fixbug/assets/test.js
@@ -1,82 +1,82 @@
 function validaSenha() {
     senha = document.getElementById("senha").value;
-    if(senha.length < 8) {
+    if (senha.length < 8) {
         alert("Senha fraca.");
         return 0
-    }
-   else {
-       console.log('passou senha');
-       return 1
+    } else {
+        console.log('passou senha');
+        return 1
     }
 }
+
 function validaEmail() {
     email = document.getElementById('email').value
-    if(email.indexOf('@') == -1) {
+    if (email.indexOf('@') == -1) {
         alert('Email inválido. Insira um email válido!');
         console.log('sem @');
         return 0
-    }
-    else {
+    } else {
         console.log('com @');
-        if(email.indexOf('.com') == -1) {
+        if (email.indexOf('.com') == -1) {
             alert('Email inválido. Insira um email válido!');
             console.log('sem .com');
-        }
-        else {
+        } else {
             console.log('passou email');
             return 1
         }
     }
 }
-function validacao() {
-    email = validaEmail();
-    senha = validaSenha();   
-    if(email == 1 && senha == 1) {
-        avancar = document.getElementsByClassName('button')[0];
-        avancar.outerHTML = '<a target="_self" href="feed.html"><button class="button" onclick="validaEmail()" onclick="validaSenha()" onclick="validacao()">Enviar</button></a>';
-        console.log('Dados válidos');
-    }
-    else {
-        console.log('Dados invalidos');
-    }
-}
-usuarios = cadastra_usuario()
+usuarios = armazena_usuario()
+
 function cadastrarUser() {
     var email = document.getElementById('email').value
     var senha = document.getElementById('senha').value
-    var senha_confirma = document.getElementById('senha_confirma');
-    
-    if(senha == senha_confirma) {
-        if(email == undefined){
+    var senha_confirma = document.getElementById('confirma_senha').value;
+
+    if (senha == senha_confirma) {
+        if (email == undefined) {
             usuarios = [{ email, senha }]
         } else {
             usuarios.push({ email, senha })
+            document.location = 'feed.html'
         }
-        localStorage.setItem("usuarios",JSON.stringify(usuarios));
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
     } else {
         alert('As senhas não são iguais')
     }
 }
-function loga_usuario() {
+
+function logar_usuario() {
     email = document.getElementById('email').value;
-    senha = docuemnt.getElementById('senha').value;
-    for(i=0; i < usuario.length; i++) {
-        if(email[i].usuarios == email){
-            if(senha[i].usuarios == senha){
-                alert('Login efetuado com sucesso');
-                document.location = '/feed.html';
+    senha = document.getElementById('senha').value;
+    for (i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].email == email) {
+            if (usuarios[i].senha == senha) {
+                console.log('Login efetuado com sucesso');
+                document.location = 'feed.html';
                 return;
-            }
-            else {
+            } else {
                 alert('Senha incorreta.');
                 return;
             }
         }
-        
+
     }
     alert('Email e senha inexistem!')
-
+    console.log(email, senha)
 }
-function armazena_usuarios(){
+
+function armazena_usuario() {
     return JSON.parse(localStorage.getItem('usuarios'))
+}
+
+function validacao() {
+    email = validaEmail();
+    senha = validaSenha();
+    if (email == 1 && senha == 1) {
+        console.log('Dados válidos');
+    } else {
+        console.log('Dados invalidos');
+    }
+    logar_usuario()
 }
